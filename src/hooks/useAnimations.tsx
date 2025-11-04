c7e712097824559fca8e82d308a7d957ebdc5633
Tim Geenen <timmiej_95@hotmail.com>
2025-11-04 11:23:29 +0900

    fix: run animations after window.onload to prevent bugs

--- src/hooks/useAnimations.tsx
+++ src/hooks/useAnimations.tsx
@@ -24,22 +24,24 @@ function useAnimations({ ...props }: Props) {
     const masterTimeline = gsap.timeline();
 
     useGSAP(() => {
-        if (props.loaded) {
-            masterTimeline.add(heroCircleAnimation());
-            masterTimeline.add(loadingPageAnimation());
-            masterTimeline.add(frontpageAnimation(), '-=25%');
-            masterTimeline.add(projectsPageAnimation());
-            masterTimeline.add(bioPageAnimation());
-            masterTimeline.add(boxGridAnimation());
-            masterTimeline.add(techPageAnimation());
-            masterTimeline.add(timelinePageAnimation());
-            masterTimeline.add(waveBackgroundAnimation());
-            masterTimeline.add(expertisePageAnimation());
-            masterTimeline.add(hobbyPageAnimation());
-            masterTimeline.add(testimonialPageAnimation());
-            masterTimeline.add(contactPageAnimation());
-            progressBarAnimation(); // keep last for correct max scroll value
-        }
+        window.onload = () => {
+            if (props.loaded) {
+                masterTimeline.add(heroCircleAnimation());
+                masterTimeline.add(loadingPageAnimation());
+                masterTimeline.add(frontpageAnimation(), '-=25%');
+                masterTimeline.add(projectsPageAnimation());
+                masterTimeline.add(bioPageAnimation());
+                masterTimeline.add(boxGridAnimation());
+                masterTimeline.add(techPageAnimation());
+                masterTimeline.add(timelinePageAnimation());
+                masterTimeline.add(waveBackgroundAnimation());
+                masterTimeline.add(expertisePageAnimation());
+                masterTimeline.add(hobbyPageAnimation());
+                // masterTimeline.add(testimonialPageAnimation());
+                masterTimeline.add(contactPageAnimation());
+                progressBarAnimation(); // keep last for correct max scroll value
+            }
+        };
     }, [props.loaded]);
 }
 
